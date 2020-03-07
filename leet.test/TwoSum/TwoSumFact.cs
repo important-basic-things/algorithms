92e8e836f270d48d7098047e3bdd640cdaa684e0
Liu Xia <lxconan@gmail.com>
2020-03-06 23:18:03 -0800

    [liuxia] feat: 001. two sum.

---
+++ leet.test/TwoSum/TwoSumFact.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Leet.TwoSum;
+using Xunit;
+
+namespace Leet.Test.TwoSum
+{
+    public class TwoSumFact
+    {
+        [SuppressMessage("ReSharper", "UnusedMember.Global")]
+        public static IEnumerable<object[]> TwoSumCases
+        {
+            get
+            {
+                yield return new object[] {new[] {2, 7, 11, 15}, 9, new[] {0, 1}};
+                yield return new object[] {new[] {3, 2, 4}, 6, new[] {1, 2}};
+                yield return new object[] {new[] {3, 2, 3}, 6, new[] {0, 2}};
+            }
+        }
+        
+        [Theory]
+        [MemberData(nameof(TwoSumCases))]
+        public void should_find_two_sum(int[] collection, int target, int[] expected)
+        {
+            Assert.Equal(expected, Solution.TwoSum(collection, target));
+        }
+    }
+}
---
+++ leet/TwoSum/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Leet.TwoSum
+{
+    public static class Solution
+    {
+        [SuppressMessage("ReSharper", "MemberCanBePrivate.Local")]
+        struct Item : IComparable<Item>
+        {
+            public readonly int Value;
+            public readonly int OriginalIndex;
+
+            public Item(int value, int originalIndex)
+            {
+                Value = value;
+                OriginalIndex = originalIndex;
+            }
+
+            public int CompareTo(Item other)
+            {
+                return Value.CompareTo(other.Value);
+            }
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Item[] sorted = CreateSortedArray(nums);
+            
+            for (int i = 0; i < sorted.Length - 1; i++)
+            {
+                Item current = sorted[i];
+                int expected = target - current.Value;
+                int indexFound = expected >= current.Value ? 
+                    Array.BinarySearch(sorted, i + 1, sorted.Length - i - 1, new Item(expected, 0)) :
+                    Array.BinarySearch(sorted, 0, i, new Item(expected, 0));
+
+                if (indexFound < 0) { continue; }
+                
+                var currentOriginalIndex = current.OriginalIndex;
+                var foundOriginalIndex = sorted[indexFound].OriginalIndex;
+                return currentOriginalIndex > foundOriginalIndex ? new[] {foundOriginalIndex, currentOriginalIndex} : new[] {currentOriginalIndex, foundOriginalIndex};
+            }
+
+            return Array.Empty<int>();
+        }
+
+        private static Item[] CreateSortedArray(int[] nums)
+        {
+            return nums.Select((value, index) => new Item(value, index))
+                .OrderBy(item => item.Value)
+                .ToArray();
+        }
+    }
+}
